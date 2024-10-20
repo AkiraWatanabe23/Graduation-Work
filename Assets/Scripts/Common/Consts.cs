48dd87476d04946e8a1e9ee51f3d101dcfd45a1e
aki_ra <a0323w@gmail.com>
2024-10-20 20:32:09 +0900

    [add] MyTemplate

---
+++ Assets/Scripts/Common/Consts.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public enum SceneName
+{
+    Title,
+    Home,
+    InGame,
+}
+
+namespace Constants
+{
+    /// <summary> 定数管理ファイル </summary>
+    public static class Consts
+    {
+        /// <summary> SEの同時再生上限 </summary>
+        public const int SEPlayableLimit = 5;
+
+        /// <summary> enumとシーン名のDictionary </summary>
+        public static readonly Dictionary<SceneName, string> Scenes = new()
+        {
+            { SceneName.Title, "TitleScene" },
+            { SceneName.Home, "HomeScene" },
+            { SceneName.InGame, "InGameScene" },
+        };
+    }
+
+    public static class ConsoleLogs
+    {
+        public static void Log(object message)
+        {
+#if UNITY_EDITOR
+            Debug.Log(message);
+#endif
+        }
+
+        public static void LogWarning(object message)
+        {
+#if UNITY_EDITOR
+            Debug.LogWarning(message);
+#endif
+        }
+
+        public static void LogError(object message)
+        {
+#if UNITY_EDITOR
+            Debug.LogError(message);
+#endif
+        }
+    }
+}
