d87251c83bc31d43e63ad2317c8997e1017c5450
吉澤 達日呼 <106291684+YoshizawaTatsuhiko@users.noreply.github.com>
2025-01-09 15:58:32 +0900

    [add] データコンテナに「初期化処理」と「ゲーム終了時に行う処理」を登録するデリゲートを作成した。 [add] オブジェクトセレクターから不要なusingを削除した。 [add] ジェンガの状態を管理するクラスの名前を変更した。

--- Assets/Scripts/Main/ObjectSelector.cs
+++ Assets/Scripts/Main/ObjectSelector.cs
@@ -9,6 +9,12 @@ public class ObjectSelector : MonoBehaviour
     private Ray _ray = new();
     private RaycastHit _hitResult = new();
     private const int MAX_RAYCAST_DISTANCE = 100;
+    private bool _isGameFinish = false;
+
+    private void OnEnable()
+    {
+        DataContainer.Instance.GameFinishRegister(GameFinish);
+    }
 
     private void Start()
     {
@@ -19,13 +25,19 @@ public class ObjectSelector : MonoBehaviour
     {
         _ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
 
-        if (DataContainer.Instance.IsGameFinish) return;
+        if (_isGameFinish) return;
         if (!Input.GetMouseButtonDown(0)) return;
         if (!Physics.Raycast(_ray, out _hitResult, MAX_RAYCAST_DISTANCE, _layerMask)) return;
         if (!_hitResult.collider.TryGetComponent(out BlockData data)) return;
 
         DataContainer.Instance.SelectedBlockId = data.BlockId;
         DataContainer.Instance.CollapseProbability = Random.Range(0f, 1f);
+    }
+
+    private void GameFinish()
+    {
+        _isGameFinish = true;
+        DataContainer.Instance.GameFinishUnregister(GameFinish);
     }
 
     private void OnDrawGizmos()
